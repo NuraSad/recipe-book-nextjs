d905fc6ca51a4a058e5647fd37255a0aef44c7bd
Nura <ainur.saduova10@gmail.com>
2025-03-17 15:27:31 -0400

    check

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <div className="text-4xl font-sans text-purple-700">
       Hello from Recipe book. We will learn Next JS, Typescript, Tailwind Css.
+      Check from the local machine.
     </div>
   );
 }
