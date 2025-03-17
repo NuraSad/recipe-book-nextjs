c16488e88217a9eee7cba9f458e67e0cddce3043
Nura <ainur.saduova10@gmail.com>
2025-03-17 18:21:46 -0400

    remove turbopack. Add typescript plugin

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col">
-    <p className="text-4xl font-sans text-purple-700 pt-6 pl-4">
-      Hello from Recipe book. We will learn Next JS, Typescript, Tailwind Css.
-      Check from the local machine.</p>
-     <Link className="text-3xl underline" href="/private">To private</Link>
-    
+      <p className="text-4xl font-sans text-purple-700 pt-6 pl-4">
+        Hello from Recipe book. We will learn Next JS, Typescript, Tailwind Css.
+        Check from the local machine.
+      </p>
+      <Link className="text-3xl underline" href="/private">
+        To private
+      </Link>
     </div>
   );
 }
